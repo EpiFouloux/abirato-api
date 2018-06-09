c9939b2e64dd7eb6c22ea0ff463ade3b483a5bda
Felix de Maneville <felix.maneville@gmail.com>
2018-06-09 14:08:31 +0200

    Added presenter folder and content to instance controller

--- app/controllers/character/instances_controller.rb
+++ app/controllers/character/instances_controller.rb
@@ -1,13 +1,20 @@
 class Character::InstancesController < ApplicationController
 
+  before_action :set_user
+  before_action :set_character, only: %i[show update destroy]
+
   # GET /users/:id/characters
   def index
-
+    characters = Character::Instance.where(user: @user)
+    json_response(
+      characters.map { |c| ApiPresenter.format(c) }
+    )
   end
 
   # GET /users/:id/characters/:id
   def show
-
+    character = Character::Instance.find(params[:id])
+    json_response(ApiPresenter.format(character))
   end
 
   # POST /users/:id/characters
@@ -23,5 +30,15 @@ class Character::InstancesController < ApplicationController
   # DELETE /users/:id/characters/:id
   def destroy
 
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
 end
