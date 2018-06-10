8ec1c5b7cf859765b4a551db45e3ec274d845091
Felix de Maneville <felix.maneville@gmail.com>
2018-06-10 14:35:03 +0200

    added routes and controllers for templates + associated tests

--- app/controllers/character/templates_controller.rb
+++ app/controllers/character/templates_controller.rb
@@ -1,2 +1,24 @@
+require_relative 'api_presenters/template'
+
 class Character::TemplatesController < ApplicationController
+  before_action :set_template, only: [:show]
+
+  # GET /templates
+  def index
+    templates = Character::Template.all
+    json_response(
+        templates.map { |n| Character::ApiPresenter::Template.format(n) }
+    )
+  end
+
+  # GET /templates/:id
+  def show
+    json_response(Character::ApiPresenter::Template.format(@template))
+  end
+
+  private
+
+  def set_template
+    @template = Character::Template.find(params[:id])
+  end
 end
