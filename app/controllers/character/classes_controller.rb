340ee64b9a4c0f9176939dea04db0294cc22b600
Felix de Maneville <felix.maneville@gmail.com>
2018-06-10 15:24:00 +0200

    added routes and controllers for classes + associated tests

--- app/controllers/character/classes_controller.rb
+++ app/controllers/character/classes_controller.rb
@@ -1,2 +1,24 @@
+require_relative 'api_presenters/class'
+
 class Character::ClassesController < ApplicationController
+  before_action :set_class, only: [:show]
+
+  # GET /classes
+  def index
+    classes = Character::Class.all
+    json_response(
+        classes.map { |c| Character::ApiPresenter::Class.format(c) }
+    )
+  end
+
+  # GET /classes/:id
+  def show
+    json_response(Character::ApiPresenter::Class.format(@class))
+  end
+
+  private
+
+  def set_class
+    @class = Character::Class.find(params[:id])
+  end
 end
